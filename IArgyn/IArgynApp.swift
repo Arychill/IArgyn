80005edc1cfbcb8e862921b373ba3e1512069bc2
Arychill <argyn.moldabek@gmail.com>
2025-03-07 01:43:49 +0500

    Initial Commit

---
+++ IArgyn/IArgynApp.swift
@@ -0,0 +1,17 @@
+//
+//  IArgynApp.swift
+//  IArgyn
+//
+//  Created by Аргын Молдабек on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct IArgynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
